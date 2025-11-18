f8600d1f253c71b56fcb97dfdfa5978611996576
nhinvo <66852256+nhinvo@users.noreply.github.com>
2025-11-18 03:06:24 +0000

    Deploying to gh-pages from @ nhinvo/nhinvo.github.io@865393a92d3cd423aad379540afca4f27a2db86c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "CV",
+  },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
           description: "",
           section: "Navigation",
           handler: () => {
